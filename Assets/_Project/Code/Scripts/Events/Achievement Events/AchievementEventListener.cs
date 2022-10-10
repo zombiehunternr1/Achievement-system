51a90af4a16e2d420ad1890655416a9bc29ab0d7
Stefan Vandenboorn <oldschoolgamernr1@gmail.com>
2022-10-10 18:02:41 +0200

    Improved achievement listener event
    Instead of having one reference to an achievement event I've now created an achievement event list.
    - In on enable I loop over all the achievement events inside the achievement events list and register them
    - In the on disable I loop over all the achievement events inside the achievement events list and unregister them

--- Assets/_Project/Code/Scripts/Events/Achievement Events/AchievementEventListener.cs
+++ Assets/_Project/Code/Scripts/Events/Achievement Events/AchievementEventListener.cs
@@ -1,23 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class AchievementEventListener : MonoBehaviour
 {
-    [SerializeField] private AchievementEvent achievementEvent;
-    [SerializeField] private UnityEvent<int, int?, float?> respondse;
+    [SerializeField] private List<AchievementEvent> _achievementEvents;
+    [SerializeField] private UnityEvent<int, int?, float?> _respondse;
 
     private void OnEnable()
     {
-        achievementEvent.RegisterListener(this);
+        foreach(AchievementEvent achievementEvent in _achievementEvents)
+        {
+            achievementEvent.RegisterListener(this);
+        }
     }
-
     private void OnDisable()
     {
-        achievementEvent.UnregisterListener(this);
+        foreach (AchievementEvent achievementEvent in _achievementEvents)
+        {
+            achievementEvent.UnregisterListener(this);
+        }
     }
-
     public void OnEventRaised(int achievementID, int? intValue, float? floatValue)
     {
-        respondse.Invoke(achievementID, intValue, floatValue);
+        _respondse.Invoke(achievementID, intValue, floatValue);
     }
 }
