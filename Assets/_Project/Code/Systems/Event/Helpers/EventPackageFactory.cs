a123596ad9fcd8dd720b6944fd20ccf53e399af2
Stefan Vandenboorn <44666850+zombiehunternr1@users.noreply.github.com>
2025-02-09 12:09:13 +0100

    Update EventPackageFactory.cs
    Removed the build and invoke batch function because it's not working for me.

--- Assets/_Project/Code/Systems/Event/Helpers/EventPackageFactory.cs
+++ Assets/_Project/Code/Systems/Event/Helpers/EventPackageFactory.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EventPackageFactory
@@ -27,37 +26,6 @@ public class EventPackageFactory
         finally
         {
             EventDataPool.Release(package);
-        }
-    }
-    public static void BuildAndInvokeBatch(EventPackage eventPackage, List<object[]> dataList)
-    {
-        if (eventPackage == null)
-        {
-            Debug.LogError("EventPackage cannot be null!");
-            return;
-        }
-        if (string.IsNullOrEmpty(eventPackage.PackageKey))
-        {
-            Debug.LogError("Package key cannot be null or empty!");
-            return;
-        }
-        List<EventData> dataBatch = new List<EventData>(dataList.Count);
-        try
-        {
-            foreach (object[] data in dataList)
-            {
-                EventData package = EventDataPool.GetEventData(eventPackage.PackageKey);
-                package.AddData(data);
-                dataBatch.Add(package);
-            }
-            eventPackage.InvokeBatch(dataBatch);
-        }
-        finally
-        {
-            foreach (EventData package in dataBatch)
-            {
-                EventDataPool.Release(package);
-            }
         }
     }
 }
